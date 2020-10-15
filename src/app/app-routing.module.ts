18cceb14d8e5ce15aade0060411341c794e1f206
David Heizer <DavidHeizer@QuickenLoans.com>
2020-10-15 10:38:17 -0400

    Routing done, components made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DevelopersComponent } from './developers/developers.component';
+import { DonutListComponent } from './donut-list/donut-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Donuts', component: DonutListComponent},
+  {path: 'Developers', component: DevelopersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DonutListComponent } from './donut-list/donut-list.component';
+import { DevelopersComponent } from './developers/developers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonutListComponent,
+    DevelopersComponent
   ],
   imports: [
     BrowserModule,
